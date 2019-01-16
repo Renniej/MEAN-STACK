2bae5a365bb5865fbcdba53f8c87f98da1e031ab
Renniej <tai.juan2@gmail.com>
2019-01-16 16:09:53 -0500

    Started mean-stack application

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ListComponent } from './components/list/list.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+
+  {path: 'create', component : CreateComponent},
+  {path: 'edit/:id', component : EditComponent},
+  {path: 'list', component: ListComponent},
+  {path: '', redirectTo : 'list', pathMatch: 'full'},
+  {path : '**', component: PageNotFoundComponent}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
